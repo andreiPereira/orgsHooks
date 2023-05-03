ee9d83e09d68507a2a0089a0268de0d5abaf955e
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-05-03 16:10:08 -0300

    Componente de estrelas #2

--- src/componentes/Estrelas.js
+++ src/componentes/Estrelas.js
@@ -1,18 +1,40 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 import estrela from '../assets/estrela.png'
 import estrelaCinza from '../assets/estrelaCinza.png'
 
-export default function Estrelas({
-    quantidade: quantidadeAntiga,
-    editavel = false,
-    grande = false,
-}) {
+export default function Estrelas({ quantidade: quantidadeAntiga, editavel = false, grande = false }) {
+
+    const [quantidade, setQuantidade] = useState(quantidadeAntiga);
+
     const estilos = estilosFuncao(grande);
+
+    const getImage = (index) => {
+        if (index < quantidade) {
+            return estrela;
+        }
+        return estrelaCinza;
+    }
+
+    const RenderEstrelas = () => {
+        const listaEstrelas = [];
+        for (let i = 0; i < 5; i++) {
+            listaEstrelas.push(
+                <TouchableOpacity
+                    key={i}
+                    onPress={() => setQuantidade(i + 1)}
+                    disabled={!editavel}
+                >
+                    <Image source={getImage(i)} style={estilos.estrela} />
+                </TouchableOpacity>
+            );
+        }
+        return listaEstrelas
+    }
+
     return <View style={estilos.estrelas}>
-        <Image source={estrela} style={estilos.estrela} />
-        <Image source={estrela} style={estilos.estrela} />
+        <RenderEstrelas />
     </View>
 }
 
