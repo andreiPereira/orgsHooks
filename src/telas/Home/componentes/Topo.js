b4e651505baac571f4b716cbb1d0f6a0f90780c9
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-05-03 08:44:36 -0300

    Consumindo o serviço

--- src/telas/Home/componentes/Topo.js
+++ src/telas/Home/componentes/Topo.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { Text, View, Image, StyleSheet } from "react-native";
 
+import { carregaTopo } from "../../../servicos/carregaDados";
 import logo from '../../../assets/logo.png'
 
-export default function Topo() {
-    return <View style={estilos.topo}>
-        <Image style={estilos.imagem} source={logo}></Image>
-        <Text style={estilos.boasVindas} >Olá Fulano</Text>
-        <Text style={estilos.legenda}>Encontre os bests produtores</Text>
-    </View>
+class Topo extends React.Component {
+
+    atualizaTopo() {
+        const retorno = carregaTopo();
+        console.log(retorno)
+    }
+
+    componentDidMount() {
+        this.atualizaTopo();
+    }
+
+    render() {
+        return <View style={estilos.topo}>
+            <Image style={estilos.imagem} source={logo}></Image>
+            <Text style={estilos.boasVindas} >Olá Fulano</Text>
+            <Text style={estilos.legenda}>Encontre os bests produtores</Text>
+        </View>
+    }
 }
 
 const estilos = StyleSheet.create({
@@ -31,3 +44,6 @@ const estilos = StyleSheet.create({
         lineHeight: 26,
     }
 });
+
+
+export default Topo
