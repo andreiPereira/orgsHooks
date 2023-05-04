77aea31e6465bc4a7717f44ec60cb3eb2c592b8d
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-05-04 08:08:59 -0300

    Hook de memória

--- src/telas/Home/componentes/Produtor.js
+++ src/telas/Home/componentes/Produtor.js
@@ -1,13 +1,21 @@
-import React, { useReducer, useState } from 'react'
+import React, { useReducer, useMemo } from 'react'
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import Estrelas from '../../../componentes/Estrelas';
 
+const distanciaEmMetros = (distancia) => {
+    return `${distancia}m`;
+}
+
 export default function Produtor({ nome, imagem, distancia, estrelas }) {
 
-    //const [selecionado, setSelecionado] = useState(false);
     const [selecionado, inverterSelecionado] = useReducer(
-        (selecionado) => !selecionado, // estado atual
-        false // valor inicial
+        (selecionado) => !selecionado,
+        false
+    );
+
+    const distanciaTexto = useMemo(
+        () => distanciaEmMetros(distancia),
+        [distancia]
     );
 
     return <TouchableOpacity style={estilos.cartao} onPress={inverterSelecionado}>
@@ -21,7 +29,7 @@ export default function Produtor({ nome, imagem, distancia, estrelas }) {
                     grande={selecionado}
                 />
             </View>
-            <Text style={estilos.distancia} >{distancia}</Text>
+            <Text style={estilos.distancia} >{distanciaTexto}</Text>
         </View>
     </TouchableOpacity>
 }
